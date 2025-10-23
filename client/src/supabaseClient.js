3a95ace533862f603d8543f9d919c1807d668e08
PipoSatoshi <emanuelalmeida11@gmail.com>
2025-10-23 20:22:23 +0100

    delete

--- client/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hrtikedecdmmacnudkgd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhydGlrZWRlY2RtbWFjbnVka2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNTU5MTYsImV4cCI6MjA3NjczMTkxNn0.2-xdqGinjbCoVlQpzXxn4CI2Ecosy3O9LFKc6OJRBpY'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
